695c7dfaa73b5cf8bd2052071a30e41ab4528d76
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-09-08 19:50:34 +0300

    add Commentary model

--- bp_manager/models.py
+++ bp_manager/models.py
@@ -33,3 +33,21 @@ class Blueprint(models.Model):
 
     def __str__(self) -> str:
         return f"{self.title} ({self.owner.username})"
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    blueprint = models.ForeignKey(
+        Blueprint,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ["-created_time"]
