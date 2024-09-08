93a38ecf14580fc6e2d2bbd765f4fdc1b8405e41
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-09-08 19:38:52 +0300

    add Tag model, changed str metod for User

--- bp_manager/models.py
+++ bp_manager/models.py
@@ -4,4 +4,11 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     def __str__(self) -> str:
-        return f"{self.username}"
+        return self.username
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
