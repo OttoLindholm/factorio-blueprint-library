8106d27b284b0f6113485db734bd01a17f7226f0
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-09-10 20:19:54 +0300

    Create BlueprintListView class

--- bp_manager/views.py
+++ bp_manager/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from bp_manager.models import Blueprint
+
+
+class BlueprintListView(ListView):
+    model = Blueprint
+    context_object_name = "blueprint_list"
+    template_name = "bp_manager/blueprint_list.html"
+    queryset = Blueprint.objects.select_related("owner")
+    paginate_by = 5
