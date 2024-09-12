4cae4584210a6e68fee629df7b555a78be79c5c8
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-09-12 22:49:28 +0300

    Add urls for Create, Update, Delete blueprints

--- bp_manager/urls.py
+++ bp_manager/urls.py
@@ -1,10 +1,35 @@
 from django.urls import path
 
-from bp_manager.views import BlueprintListView, BlueprintDetailView
+from bp_manager.views import (
+    BlueprintListView,
+    BlueprintDetailView,
+    BlueprintCreateView,
+    BlueprintUpdateView,
+    BlueprintDeleteView,
+)
 
 urlpatterns = [
     path("", BlueprintListView.as_view(), name="index"),
-    path("blueprints/<int:pk>/", BlueprintDetailView.as_view(), name="blueprint-detail"),
+    path(
+        "blueprints/<int:pk>/",
+        BlueprintDetailView.as_view(),
+        name="blueprint-detail"
+    ),
+    path(
+        "blueprints/create/",
+        BlueprintCreateView.as_view(),
+        name="blueprint-create"
+    ),
+    path(
+        "blueprints/<int:pk>/update/",
+        BlueprintUpdateView.as_view(),
+        name="blueprint-update"
+    ),
+    path(
+        "blueprints/<int:pk>/delete/",
+        BlueprintDeleteView.as_view(),
+        name="blueprint-delete"
+    ),
 ]
 
 app_name = "bp_manager"
