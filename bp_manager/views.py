4dc60803660a22fbb4c8c4a62ed7e233c521a493
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-09-12 15:48:46 +0300

    Create BlueprintDetailView & change pagination limit to 6

--- bp_manager/views.py
+++ bp_manager/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView
 
 from bp_manager.models import Blueprint
 
@@ -8,4 +9,10 @@ class BlueprintListView(ListView):
     context_object_name = "blueprint_list"
     template_name = "bp_manager/blueprint_list.html"
     queryset = Blueprint.objects.select_related("owner")
-    paginate_by = 5
+    paginate_by = 6
+
+
+class BlueprintDetailView(LoginRequiredMixin, DetailView):
+    model = Blueprint
+    template_name = "bp_manager/post_detail.html"
+    context_object_name = "blueprint"
