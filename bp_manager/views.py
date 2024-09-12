31a5cbb11a2fbae881c878eaf1d55a00dcf7beed
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-09-12 19:07:23 +0300

    Add BlueprintUpdateView, only owner of Blueprint can update it

--- bp_manager/views.py
+++ bp_manager/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseForbidden
 from django.shortcuts import redirect
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from bp_manager.forms import CommentaryForm, BlueprintForm
 from bp_manager.models import Blueprint, Commentary
@@ -44,6 +45,24 @@ class BlueprintDetailView(LoginRequiredMixin, DetailView):
 class BlueprintCreateView(LoginRequiredMixin, CreateView):
     model = Blueprint
     form_class = BlueprintForm
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
+
+class BlueprintUpdateView(LoginRequiredMixin, UpdateView):
+    model = Blueprint
+    form_class = BlueprintForm
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj.user != self.request.user:
+            return HttpResponseForbidden("You are not allowed to edit this object.")
+        return obj
 
     def form_valid(self, form):
         form.instance.user = self.request.user
