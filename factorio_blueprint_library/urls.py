44de03f8671336b2598240814b140844860487fe
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-09-12 18:31:57 +0300

    Add MEDIA_URL to project urlpatterns

--- factorio_blueprint_library/urls.py
+++ factorio_blueprint_library/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("bp_manager.urls", namespace="bp_manager")),
     path("accounts/", include("django.contrib.auth.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
