23c26144ffc6b14c6786047f7fdf81326d332963
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-09-24 22:45:44 +0300

    Create .env.sample, add secret key & debug to .env, add python-dotenv to requirements

--- factorio_blueprint_library/settings.py
+++ factorio_blueprint_library/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,11 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-s0(i%y$3*nuuc!d)wuw$xs0yibggywgjn*9irjs9mmzte!c2p^"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = []
 
@@ -103,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "bp_manager.User"
 LOGIN_REDIRECT_URL = "/"
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
