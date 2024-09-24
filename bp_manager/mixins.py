f0a25ccbae5ff8657a4da516f06911d1f4ba91d7
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-09-24 03:13:07 +0300

    Create user detail template, new check for UserIsOwnerMixin for users update/delete

--- bp_manager/mixins.py
+++ bp_manager/mixins.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponseForbidden
 
+from bp_manager.models import User
+
 
 class UserIsOwnerMixin:
-
     def get_object(self, queryset=None):
         obj = super().get_object(queryset)
-        if obj.user != self.request.user:
+
+        if isinstance(obj, User):
+            if obj != self.request.user:
+                return HttpResponseForbidden("You are not allowed to modify this user.")
+            return obj
+
+        if hasattr(obj, 'user') and obj.user != self.request.user:
             return HttpResponseForbidden("You are not allowed to modify this object.")
+
         return obj
