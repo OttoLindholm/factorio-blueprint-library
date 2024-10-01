21a18f644c6b49cc33e0d7cee2cf62d1e9e9bbe2
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-02 01:30:21 +0300

    return build.sh to first configuration

--- build.sh
+++ build.sh
@@ -5,12 +5,6 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Ensure the media directory exists
-mkdir -p media
-
-# Copy media files from the existing directory
-cp -r media/* media/
-
 # Convert static asset files
 python manage.py collectstatic --no-input
 
@@ -19,6 +13,3 @@ python manage.py migrate
 
 # Load demo data
 python manage.py loaddata demo_data.json
-
-# Start the server
-gunicorn yourproject.wsgi:application
