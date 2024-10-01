42c2d0ae50917ebf00f050bcf7dfbfa9de95e55d
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-01 23:48:09 +0300

    change ALLOWED_HOSTS

--- factorio_blueprint_library/settings.py
+++ factorio_blueprint_library/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://factorio-blueprint-library.onrender.com/"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "factorio-blueprint-library.onrender.com"]
 
 # Application definition
 
