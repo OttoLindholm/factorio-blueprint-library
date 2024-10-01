10994312ce721f295b2f681ea76cae0f897776a6
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-01 22:57:45 +0300

    Add whitenoise to requirements, add dir for staticfiles

--- factorio_blueprint_library/settings.py
+++ factorio_blueprint_library/settings.py
@@ -50,6 +50,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -130,7 +131,7 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "staticfiles/"
 
 # MEDIA_URL defines the public URL of uploaded media files
 
