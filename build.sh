97060607da7a669369e7146b321e94b65e6e6762
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-02 23:50:54 +0300

    Modify build script for managing demo media files
    Move demo media files before collect static
    
    Update moving media files
    
    Update path for demo_media in build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+# Move the media folder from static files to the project level
+mkdir -p "media"
+mv "static/demo_media/"* "media"
+echo "Media folder successfully moved to the project level."
+
 # Convert static asset files
 python manage.py collectstatic --no-input
 
